aab1304d1b5571e0bed32cc0bcb6e5bdb37fecbb
Cole Harper <Cole.r.n.Harper@gmail.com>
2025-01-03 15:02:30 -0800

    moved from using a CLI approach to using a localhost server with a single GET and POST

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,26 +1,58 @@
 mod gcd;
 
-use std::str::FromStr;
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use crate::gcd::gcd;
 
-fn main() {
-
-    let mut numbers = Vec::new();
-
-    for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
-    }
-
-    if numbers.len() == 0 {
-        eprintln!("Usage: gcd NUMBER ...");
-        std::process::exit(1);
-    }
-
-    let mut n = numbers[0];
-    for m in &numbers[1..] {
-        n = gcd(n, *m);
-    }
-
-    println!("The gcd of {:?} is {}", numbers, n);
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
 }
+
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
+}
+
+#[actix_web::main]
+async fn main() {
+   let server = HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(get_index))
+        .route("/gcd", web::post().to(post_gcd))
+    });
+
+    println!("Serving on http://localhost:3000");
+    server.bind("127.0.0.1:3000")
+        .expect("Failed to start server")
+        .run().await.expect("error running server");
+
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+is <b>{}</b>\n", form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+    .content_type("text/html")
+    .body(response)
+}
+
